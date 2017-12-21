7cdb4e0ac7aa0d7d22962e2623deaa34f17821fb
Hiid <skatkdwo233@likelion.org>
2017-12-21 10:06:47 +0900

    1

---
+++ test.py
@@ -0,0 +1,12 @@
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# plt.plot(np.random.randn(10))
+# plt.show()
+
+
+
+testlist = ['a','b','c','d','e']
+
+for i, variable in enumerate(testlist):
+	print (i,variable)
